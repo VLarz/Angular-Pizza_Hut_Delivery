b6105d47598299f65aedf997e8b7e2c70f3a0fb6
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-12-29 15:07:53 +0800

    Added method to get json data

--- src/app/components/services/pizza-order.service.ts
+++ src/app/components/services/pizza-order.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment';
+import { PizzaOrder } from '../models/pizza-order.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaOrderService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPizzaOrders(): Observable<PizzaOrder> {
+    return this.http.get<PizzaOrder>(`${environment.jsonPath}/pizza-orders.json`);
+  }
 }
