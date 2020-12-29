d3388478f52ef194c5d87f2f8d7fb108a18085d7
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-12-29 14:51:34 +0800

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PizzaComponent } from './components/pizza/pizza.component';
+import { PizzaOrderListComponent } from './components/pizza/pizza-order-list/pizza-order-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaComponent,
+    PizzaOrderListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pizza/pizza-order-list/pizza-order-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-order-list',
+  templateUrl: './pizza-order-list.component.html',
+  styleUrls: ['./pizza-order-list.component.scss']
+})
+export class PizzaOrderListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
